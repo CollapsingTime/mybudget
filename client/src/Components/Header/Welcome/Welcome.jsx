0ff9e18555e14aae727a38cc5848836604d87b0e
Vladislav Rumiantsev <97826421+CollapsingTime@users.noreply.github.com>
2022-07-14 22:24:20 +0300

    Update Welcome.jsx

--- client/src/Components/Header/Welcome/Welcome.jsx
+++ client/src/Components/Header/Welcome/Welcome.jsx
@@ -25,7 +25,7 @@ const Welcome = () => {
       <RowTwo>
         <ExploreBtn>
           <Link spy={true} smooth={true} to="MainPage">
-            <OutlineButton href="#mainPage">Обзор</OutlineButton>
+            <OutlineButton href="#MainPage">Обзор</OutlineButton>
           </Link>
         </ExploreBtn>
         <ContactBtn>
